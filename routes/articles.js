b7b3985c46c358642033e98ba0d114c0f87dd120
Brandon Altamirano <manabran501@hotmail.com>
2023-04-09 22:37:04 -0700

    Simple login system with hardcoded username and password on .env file. This is to implement dellement delete and edit functions for blog posts. Incorrect username and password info redirects to login page. Needpage. Need to hash password for production purposes.

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const express = require('express')
+const session = require('express-session')
 const Article = require('./../models/article')
 const router = express.Router()
+let loggedIn = false;
+const adminUsername = process.env.ADMIN_USERNAME 
+const adminPassword = process.env.ADMIN_PASSWORD
 
 router.get('/', async (req, res) => {
     const articles = await Article.find().sort({
@@ -9,13 +14,50 @@ router.get('/', async (req, res) => {
     res.render('articles/blog', {articles: articles})
 })
 
+router.get('/login', (req, res) => {
+    res.render('articles/login');
+})
+
+router.post('/auth', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (username && password) {
+        if (username == adminUsername && password == adminPassword) {
+            loggedIn = true;
+            res.redirect('/blog/admin');
+        }
+        else {
+            console.log("Incorrect username or password")
+            res.redirect('/blog/login');
+        }
+    }
+})
+
+router.get('/admin', async (req, res) => {
+    if (loggedIn == true) {
+        const articles = await Article.find().sort({
+            createdAt: 'desc'
+        })
+        res.render('articles/admin', {articles: articles});
+    }
+    else {
+        res.redirect('/blog/login');
+    }
+})
 router.get('/new', (req, res) => {
-    res.render('articles/new', { article: new Article()})
+    if (loggedIn == true) {
+        res.render('articles/new', { article: new Article()});
+    }
+    else {
+        res.redirect('/blog/login');
+    }
+    
 })
 
 router.get('/:slug', async (req, res) => {
     const article = await Article.findOne({ slug: req.params.slug })
-    if (article == null) res.redirect('/')
+    if (article == null) res.redirect('/blog')
     res.render('articles/show', { article: article })
 })
 
