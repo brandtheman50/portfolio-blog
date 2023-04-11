8df3c44b415609bd936a588fc6055ff13e5f7250
Brandon <manabran501@hotmail.com>
2023-04-11 16:35:41 -0700

    Adding try catch blocks to each async function to catch errors.

--- server.js
+++ server.js
@@ -18,4 +18,5 @@ app.get('/', (req, res) => {
 
 app.use('/blog', articleRouter);
 
-app.listen(5000);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Server is running on port ' + port))
