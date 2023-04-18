525e8628616abde493c2c3ad2aaa61e2619a6160
Brandon <manabran501@hotmail.com>
2023-04-17 22:38:27 -0700

    Edit function not fully functioning. POST method not defined.

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,7 @@ const express = require('express')
 const session = require('express-session')
 const Article = require('./../models/article')
 const router = express.Router()
-let loggedIn = false;
+let loggedIn = true;
 const adminUsername = process.env.ADMIN_USERNAME 
 const adminPassword = process.env.ADMIN_PASSWORD
 
@@ -61,19 +61,37 @@ router.get('/:slug', async (req, res) => {
     res.render('articles/show', { article: article })
 })
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    try{
-        article = await article.save()
-        res.redirect(`/blog/${article.slug}`)
-    } catch (e) {
-        console.log(e)
-        res.render('/')
-    }
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', { article: article})
 })
 
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next()
+}, saveArticleAndRedirect('edit'))
+
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title 
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+        try {
+            article = await article.save()
+            res.redirect(`/blog/${article.slug}`)
+        } catch (e) {
+            res.render(`articles/${path}`, { article: article})
+        }
+    }
+}
 module.exports = router
